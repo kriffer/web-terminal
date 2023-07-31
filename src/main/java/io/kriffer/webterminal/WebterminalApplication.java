9f01c75c4dd41a66fc5b78b3d31012356bdb9849
kriffer <antonio.kravets@gmail.com>
2023-07-31 12:11:29 +0300

    initial

---
+++ src/main/java/io/kriffer/webterminal/WebterminalApplication.java
@@ -0,0 +1,13 @@
+package io.kriffer.webterminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebterminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebterminalApplication.class, args);
+	}
+
+}
